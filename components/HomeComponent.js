4e0e6b6a5f143787fade39b908b507a7d7aef22d
Monica Salamanca <moni.salamanca@gmail.com>
2020-05-20 20:54:21 -0400

    added font awesome, added icons and style web app

--- components/HomeComponent.js
+++ components/HomeComponent.js
@@ -3,6 +3,8 @@ import styled from 'styled-components';
 import useTranslation from '../hooks/useTranslations';
 import Icons from '../components/Icons.js';
 import ContactForm from '../components/ContactForm';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faTools, faLaptopCode, faCode, faBriefcase, faAddressCard, faLightbulb } from '@fortawesome/free-solid-svg-icons'
 
 const HomeStyle = styled.div`
   color: #292929;
@@ -18,7 +20,7 @@ const HomeStyle = styled.div`
 
   h1 {
     margin-top: 32px;
-    font-size: 32px;
+    font-size: 18px;
     word-break: 'break-word';
     font-family: 'Montserrat', sans-serif;
     font-weight: 900;
@@ -32,9 +34,74 @@ const HomeStyle = styled.div`
     font-family: 'Quicksand', sans-serif;
     margin-top: 20px;
     text-align: center;
-    font-size: 20px;
-    line-height: 26px;
+    font-size: 16px;
+    line-height: 24px;
     color: #404040;
+  }
+`
+
+const SectionStyle = styled.div`
+  border: 1px solid #bcbaba;
+  border-radius: 5px;
+  padding: 10px;
+  box-shadow: 3px 3px 5px 0px rgba(201,201,201,1);
+  margin-bottom: 36px;
+  margin-top: 24px;
+  padding: 36px 10px 24px;
+
+  &:first-child {
+    margin-top: 0;
+  }
+
+  .logo-style {
+    img {
+
+
+      &.me-img {
+        border-radius: 100px;
+        border: 1px solid #bcbaba;
+
+
+      }
+    }
+  }
+
+  span {
+    background-color: #ff4c4c;
+    padding: 4px 10px;
+    color: #fff;
+    border-radius: 8px;
+    font-family: monospace;
+    display: inline-flex;
+    max-width: fit-content;
+    margin: 3px;
+  }
+
+  .icon-hero {
+    display: flex;
+    justify-content: center;
+
+    .icon-awesome {
+      font-size: 80px;
+      padding: 20px;
+      border: 1px solid #cfcfcf;
+      border-radius: 80px;
+      color: #808080;
+
+      &.lightbulb {
+        padding: 20px 32px;
+      }
+
+      &.code {
+        padding: 22px 13px;
+        font-size: 75px;
+      }
+
+      &.contact {
+        padding: 21px 17px;
+        font-size: 78px;
+      }
+    }
   }
 `
 
@@ -44,17 +111,68 @@ const HomeComponent = () => {
 
   return (
     <HomeStyle>
-      <div className='logo-style'>
-        <img src="/img/salmon.png" alt="Logo" width="232" height="170" />
-      </div>
-      <p>{t('description')}</p>
-      <h1>{t('skills')}</h1>
-      <p>&middot; Javascript &middot; HTML5 &middot; CSS &middot; Git &middot; SASS &middot; Gulp &middot; ReactJS &middot; NextJS &middot; Webpack &middot; Babel &middot; ES6 &middot; PHP &middot; Laravel &middot; Photoshop &middot; Wordpress &middot; MySQL &middot; Agile &middot; NodeJS &middot;</p>
-      <h1>{t('code')}</h1>
-      <Icons />
-      <h1>{t('myPortfolio')}</h1>
-      <p>Add Name of Project - Description - Stack used - Link to code - Link to Video on Youtube of how it was build - Link to video on how to </p>
-      <ContactForm />
+
+      <SectionStyle>
+        <div className='logo-style'>
+          {/* <img src="/img/salmon.png" alt="Logo" width="232" height="170" /> */}
+          <img className="me-img" src="/img/me.jpg" alt="Logo" width="140" height="140" />
+        </div>
+        <p>{t('description')}</p>
+      </SectionStyle>
+      <SectionStyle>
+        <div className="icon-hero">
+          <FontAwesomeIcon className="icon-awesome" icon={faTools} />
+        </div>
+        
+        <h1>{t('skills')}</h1>
+        <p>
+          <span>#Javascript</span>
+          <span>#HTML5</span>
+          <span>#CSS</span>
+          <span>#Git</span>
+          <span>#SASS</span>
+          <span>#Gulp</span>
+          <span>#ReactJS</span>
+          <span>#NextJS</span>
+          <span>#Webpack</span>
+          <span>#Babel</span>
+          <span>#ES6</span>
+          <span>#PHP</span>
+          <span>#Laravel</span>
+          <span>#Photoshop</span>
+          <span>#Wordpress</span>
+          <span>#MySQL</span>
+          <span>#Agile</span>
+          <span>#NodeJS</span>
+        </p>
+      </SectionStyle>
+      <SectionStyle>
+        <div className="icon-hero">
+          <FontAwesomeIcon className="icon-awesome lightbulb" icon={faLightbulb} />
+        </div>
+        <h1>{t('learning')}</h1>
+        <p>Udemy Courses Details</p>
+      </SectionStyle>
+      <SectionStyle>
+        <div className="icon-hero">
+          <FontAwesomeIcon className="icon-awesome code" icon={faCode} />
+        </div>
+        <h1>{t('code')}</h1>
+        <Icons />
+      </SectionStyle>
+      <SectionStyle>
+        <div className="icon-hero">
+          <FontAwesomeIcon className="icon-awesome" icon={faBriefcase} />
+        </div>
+        <h1>{t('myPortfolio')}</h1>
+        <p>Add Name of Project - Description - Stack used - Link to code - Link to Video on Youtube of how it was build - Link to video on how to </p>
+      </SectionStyle>
+      <SectionStyle>
+        <div className="icon-hero">
+          <FontAwesomeIcon className="icon-awesome contact" icon={faAddressCard} />
+        </div>
+        <ContactForm />
+      </SectionStyle>
     </HomeStyle> 
   )
 
--- translations/strings.js
+++ translations/strings.js
@@ -4,6 +4,7 @@ const strings = {
     myPortfolio: 'Portfolio',
     description: 'Hi! My name is Monica and I am a creative and detailed oriented Frontend Developer with a bachelor degree in software engineer. I am passionate about HTML, CSS, JavaScript, UX designs and trending technologies, which over the course of my career I have used to build many websites and web applications in different fields. ',
     skills: 'Skills',
+    learning: 'Currently learning...',
     contact: 'Contact me',
     code: 'Some code...'
   },
@@ -12,6 +13,7 @@ const strings = {
     myPortfolio: 'Portefeuille', 
     description: 'Salut!, Mon nom es Monica, Je suis une creative Frontend developeur avec une Bac en ingénieur logiciel. Je suis passione de HTML, CSS, Javascript, conception de UX et technologies tendance, dans le cours de ma carriere javais utiliser pour programmer des sites web et des differents applications dans differentes domaines. ',
     skills: 'Compétences',
+    learning: 'En cours d\'apprentissage...',
     contact: 'Contactez moi',
     code: 'Du code...'
   },
@@ -20,6 +22,7 @@ const strings = {
     myPortfolio: 'Portafolio',
     description: 'Hola! Mi nombre es Monica y soy una creativa programadora Frontend con un bachelor en ingeniria de software. Me apasiona el HTML, CSS, Javascript, diseño UX y ultimas tendencias en tecnología, que dentro del curso de mi carrera he utilizado para programar websites y aplicaciones web en diferentes campos. ',
     skills: 'Habilidades',
+    learning: 'Actualmente aprendiendo...',
     contact: 'Contáctame',
     code: 'Algo de codigo'
   }
