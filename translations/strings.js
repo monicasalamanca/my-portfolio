eef2c84b9d0cc2245701ea8aa5945c2f70c3bab4
Monica Salamanca <moni.salamanca@gmail.com>
2020-06-24 21:16:24 -0400

    changed the title in head tag

--- translations/strings.js
+++ translations/strings.js
@@ -1,6 +1,6 @@
 const strings = {
   en: {
-    homePageTitle: 'Salmon - Home Page',
+    homePageTitle: 'Monica Salamanca - Home Page',
     myPortfolio: 'Portfolio',
     description: 'Hi! My name is Monica and I am a creative and detailed oriented Frontend Developer with a bachelor degree in software engineer. I am passionate about HTML, CSS, JavaScript, UX designs and trending technologies, which over the course of my career I have used to build many websites and web applications in different fields. ',
     skills: 'Skills',
@@ -14,7 +14,7 @@ const strings = {
     projectmyportfolio: 'My portfolio'
   },
   fr: {
-    homePageTitle: 'Salmon - Page d`\accueil',
+    homePageTitle: 'Monica Salamanca - Page d`\accueil',
     myPortfolio: 'Portefeuille', 
     description: `Salut!, Je m'appelle Monica, Je suis une creative Frontend developeur avec une Bac en génie logiciel. Je suis passione de HTML, CSS, Javascript, conception de UX et tendances informatiques que dans le cours de ma carriere j'avais utiliser pour programmer des sites web et des differents applications dans differentes domaines. `,
     skills: 'Compétences',
@@ -28,7 +28,7 @@ const strings = {
     projectmyportfolio: 'Mon Portefeuille'
   },
   es: {
-    homePageTitle: 'Salmon - Home Page',
+    homePageTitle: 'Monica Salamanca - Home Page',
     myPortfolio: 'Portafolio',
     description: 'Hola! Mi nombre es Monica y soy una creativa programadora Frontend con un bachelor en ingeniria de software. Me apasiona el HTML, CSS, Javascript, diseño UX y ultimas tendencias en tecnología, que dentro del curso de mi carrera he utilizado para programar websites y aplicaciones web en diferentes campos. ',
     skills: 'Habilidades',
